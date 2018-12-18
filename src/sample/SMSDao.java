8ddda63a1614a1025c3b859d2f49382869443ab5
lihao <leeboco@foxmail.com>
2018-12-19 03:06:46 +0800

    SMSBOM~

---
+++ src/sample/SMSDao.java
@@ -0,0 +1,31 @@
+package sample;
+
+public class SMSDao {
+    private String url;
+    private String inputId;
+    private String sendId;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getInputId() {
+        return inputId;
+    }
+
+    public void setInputId(String inputId) {
+        this.inputId = inputId;
+    }
+
+    public String getSendId() {
+        return sendId;
+    }
+
+    public void setSendId(String sendId) {
+        this.sendId = sendId;
+    }
+}
